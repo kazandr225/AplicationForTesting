8da08bdc51c38a783a3b77316ec51b7e5adec308
kazandr225 <danii.kazanskiy@mail.ru>
2023-05-14 21:38:06 +0300

    Закончена работа над авторизацией и регистрацией

--- AplicationForTesting/Pages/AutorizationPage.xaml.cs
+++ AplicationForTesting/Pages/AutorizationPage.xaml.cs
@@ -38,7 +38,7 @@ namespace AplicationForTesting.Pages
 
             Users users = BaseClass.EM.Users.FirstOrDefault(x => x.UserLogin == tbLogin.Text && x.UserPassword == tbPassword.Text);
 
-            if (users == null)
+            if (users == null) // эта штука работает, если даже пустые значения ввести
             { 
                 MessageBox.Show("Проверьте введенные данные", "Пользователь не найден!", MessageBoxButton.OK); 
             }
@@ -49,11 +49,11 @@ namespace AplicationForTesting.Pages
                 switch (users.RoleID) //проверка на роль, если пользоваетль найден
                 {
                     case 1: //Студент
-                        MessageBox.Show("Добро пожаловать, клиент");
+                        MessageBox.Show("Добро пожаловать!");
                         FrameClass.MainFrame.Navigate(new PracticPage1(users));
                         break;
                     case 2: //Преподаватель
-                        MessageBox.Show("Добро пожаловать, администратор");
+                        MessageBox.Show("Добро пожаловать!");
                         FrameClass.MainFrame.Navigate(new ResoultsPage(users));
                         break;
                 }
@@ -63,6 +63,19 @@ namespace AplicationForTesting.Pages
         private void btnRegistration_Click(object sender, RoutedEventArgs e)
         {
             FrameClass.MainFrame.Navigate(new RegistrationPage());
+        }
+
+        /// <summary>
+        /// Очистка полей при нажатии на них
+        /// </summary>
+        private void TexBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbLogin.Text == "Логин" || tbPassword.Text == "Пароль")
+            {
+                TextBox tb = (TextBox)sender;
+                tb.Text = string.Empty;
+                tb.GotFocus -= TexBox_GotFocus;
+            }
         }
     }
 }
--- AplicationForTesting/Pages/RegistrationPage.xaml.cs
+++ AplicationForTesting/Pages/RegistrationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AplicationForTesting.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,111 @@ namespace AplicationForTesting.Pages
         public RegistrationPage()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Метод для регистрации пользователей и проверки введенных данных
+        /// </summary>
+        private void btnRegistranion_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                //Проверка на логин. Если введенный логин будет соответствовать уже существующему, то выведется соответстввующее сообщение
+                var regUser = BaseClass.EM.Users.FirstOrDefault(x => x.UserLogin == tbLogin.Text);
+                if (regUser != null)
+                {
+                    MessageBox.Show("Пожалуйста, придумайте другой логин", "Этот логин занят!");
+                }
+
+                //объект для записей в БД
+                Users usersTable = new Users()
+                {
+                    UserSurname = tbSurname.Text,
+                    UserName = tbName.Text,
+                    UserPatronymic = tbPatronymic.Text,
+
+                    UserLogin = tbLogin.Text,
+                    UserPassword = tbPassword.Text,
+
+                    RoleID = 1
+                };
+                BaseClass.EM.Users.Add(usersTable);
+                BaseClass.EM.SaveChanges();
+                MessageBox.Show("Вы зарегистрировались!", "Регистрация прошла успешно");
+                FrameClass.MainFrame.Navigate(new AutorizationPage());
+            }
+
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так!", "Проверьте правильность введенных данных", MessageBoxButton.OK, MessageBoxImage.Warning);            
+            }
+
+        }
+
+        /// <summary>
+        /// Назад на начальную страницу
+        /// </summary>
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            FrameClass.MainFrame.Navigate(new AutorizationPage());
+        }
+
+        //Потом в общий метод отчистки переделать
+        private void tbSurname_MouseDown(object sender, MouseEventArgs e)
+        {
+            //if (tbSurname.Text == "Фамилия")
+            //{
+            //    tbSurname.Clear();
+            //}
+
+            tbSurname.Clear();
+        }
+
+        private void tbName_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (tbName.Text == "Имя")
+            {
+                tbName.Clear();
+            }
+        }
+
+        private void tbPatronymic_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (tbPatronymic.Text == "Отчество")
+            {
+                tbPatronymic.Clear();
+            }
+        }
+
+        private void tbLogin_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (tbLogin.Text == "Логин")
+            {
+                tbPatronymic.Clear();
+            }
+        }
+
+        private void tbPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (tbPassword.Text == "Пароль")
+            {
+                tbPassword.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Очистка полей при нажатии на них
+        /// </summary>
+        private void TexBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbSurname.Text == "Фамилия" || tbName.Text == "Имя" || tbPatronymic.Text == "Отчество" || tbLogin.Text == "Логин" || tbPassword.Text == "Пароль")
+            {
+                TextBox tb = (TextBox)sender;
+                tb.Text = string.Empty;
+                tb.GotFocus -= TexBox_GotFocus;
+            }
+
         }
     }
 }
